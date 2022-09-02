f75710eeb2c9206275db64c21d9e1f511a1d8857
collinjones <cojo2657@colorado.edu>
2022-09-02 11:43:45 -0600

    added a single bandname db checker for duplicates

--- Bandnames/views.py
+++ Bandnames/views.py
@@ -1,25 +1,37 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Bandname
+from django.core.exceptions import ObjectDoesNotExist
 
 def create(request):
     
     if request.method == 'POST':
-        bandname = request.POST['bandname']
-        username = request.POST['username']
-        success = 'The bandname "' + bandname + '" submitted successfully by ' + username
 
-        Bandname.objects.all().delete()
+        bn = request.POST['bandname']
 
-    
-        new_bandname = Bandname(bandname=bandname,
-                                upvotes=0,
-                                downvotes=0,
-                                username=username)
-        new_bandname.save()
-        success = 'The bandname "' + bandname + '" submitted successfully by ' + username
-        return HttpResponse(success)
+        # Return a failed response if bandname exists in DB already 
+        try:
+            if (Bandname.objects.get(bandname = bn)):
+                failed = "Name exists already!"
+                return HttpResponse(failed)  
 
+        # If the bandname does not exist, create it
+        except Bandname.DoesNotExist:
+            username = request.POST['username']
+            success = 'The bandname "' + bn + '" submitted successfully by ' + username
+            # Bandname.objects.all().delete()
+
+            new_bandname = Bandname(bandname=bn,
+                                    upvotes=0,
+                                    downvotes=0,
+                                    username=username)
+            new_bandname.save()
+            success = 'The bandname "' + bn + '" submitted successfully by ' + username
+            return HttpResponse(success)
+            
+        
+            
 
 def index(request):
     ctxt = {"title" : "Submission Page"}
