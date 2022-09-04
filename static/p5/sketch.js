b2a049cd03f95b5d19378886744f57353b655441
collinjones <cojo2657@colorado.edu>
2022-09-04 14:52:44 -0600

    committing p5 sketch

--- static/p5/sketch.js
+++ static/p5/sketch.js
@@ -1,33 +1,89 @@
-let angle = 0;
+
+// Spring drawing constants for top bar
+let springHeight = 32,
+    left,
+    right,
+    maxHeight = 200,
+    minHeight = 100,
+    over = false,
+    move = false;
+
+// Spring simulation constants
+let M = 0.8,  // Mass
+    K = 0.2,  // Spring constant
+    D = 0.92, // Damping
+    R = 150;  // Rest position
+
+// Spring simulation variables
+let ps = R,   // Position
+    vs = 0.0, // Velocity
+    as = 0,   // Acceleration
+    f = 0;    // Force
 
 function setup() {
     var canvas = createCanvas(400, 400);
     canvas.parent('sketch-holder');
-    rectMode(CENTER);
-    angleMode(DEGREES);
+    rectMode(CORNERS);
     noStroke();
+    left = width / 2 - 100;
+    right = width / 2 + 100;
 }
 
 function draw() {
-    background(220);
-
-    // Red square at (100, 100)
-    push();
-    translate(100, 100);
-    rotate(angle);
-    fill('red');
-    rect(0, 0, 50, 50);
-    pop();  //The origin is back to (0, 0) and rotation is back to 0.
-
-    // Green square at (300, 300)
-    push();
-    translate(300, 300);
-    rotate(-1 * angle);
-    fill('green');
-    rect(0, 0, 50, 50);
-    pop();
-
-    angle += 1;
+    background(102);
+    updateSpring();
+    drawSpring();
 }
 
+function drawSpring() {
+    // Draw base
+    fill(0.2);
+    let baseWidth = 0.5 * ps + -8;
+    rect(width / 2 - baseWidth, ps + springHeight, width / 2 + baseWidth, height);
 
+    // Set color and draw top bar
+    if (over || move) {
+        fill(255);
+    } else {
+        fill(204);
+    }
+
+    rect(left, ps, right, ps + springHeight);
+}
+
+function updateSpring() {
+    // Update the spring position
+    if (!move) {
+        f = -K * (ps - R); // f=-ky
+        as = f / M;          // Set the acceleration, f=ma == a=f/m
+        vs = D * (vs + as);  // Set the velocity
+        ps = ps + vs;        // Updated position
+    }
+
+    if (abs(vs) < 0.1) {
+        vs = 0.0;
+    }
+
+    // Test if mouse if over the top bar
+    if (mouseX > left && mouseX < right && mouseY > ps && mouseY < ps + springHeight) {
+        over = true;
+    } else {
+        over = false;
+    }
+
+    // Set and constrain the position of top bar
+    if (move) {
+        ps = mouseY - springHeight / 2;
+        ps = constrain(ps, minHeight, maxHeight);
+    }
+}
+
+function mousePressed() {
+    if (over) {
+        move = true;
+    }
+}
+
+function mouseReleased() {
+    move = false;
+}
