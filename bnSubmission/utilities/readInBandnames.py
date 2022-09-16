e34e6e08c30c67e98f5c12c6faa1e321049fecc7
IDConnelly <iandconnelly@gmail.com>
2022-09-15 20:29:21 -0700

    Batch script debugging

--- bnSubmission/utilities/readInBandnames.py
+++ bnSubmission/utilities/readInBandnames.py
@@ -10,12 +10,18 @@ file = open(file_path)
 userSubmissions = str
 
 #Function to read in user list of bandnames
-def readInList(bandnamesBatch):
+def readInList(bandnamesBatch, userBool):
     data = bandnamesBatch.read()
     userSubmissions = data.splitlines()
 
-    for x in userSubmissions:
-        print(x)
+
+    if (userBool == True):
+        x = 0
+        while (x <= len(userSubmissions)):
+            print('Old value: ', userSubmissions[x])
+            userSubmissions[x] = userSubmissions[x].lstrip('1234567890. ')
+            print('New value: ', userSubmissions[x])
+            x += 1
 
     #print(userSubmissions)
     #print(len(userSubmissions))
