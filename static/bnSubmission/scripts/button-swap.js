b792d295dd06ed9c171478490e11ebc19c215322
collinjones <cojo2657@colorado.edu>
2022-09-23 17:39:58 -0600

    fixed bug where clicking anywhere would press down the submit button animation

--- static/bnSubmission/scripts/button-swap.js
+++ static/bnSubmission/scripts/button-swap.js
@@ -1,7 +1,9 @@
-document.onmousedown = function () {
-    console.log('setting clicked')
-    document.getElementById("bandname-submit").src="static/images/button-clicked.png"
+
+function clicked () {
+    var submit_button = document.getElementById("bandname-submit")
+    submit_button.src="static/images/button-clicked.png"
 }
-document.onmouseup = function () {
-    document.getElementById("bandname-submit").src="static/images/button-open.png"
+function resting () {
+    var submit_button = document.getElementById("bandname-submit")
+    submit_button.src="static/images/button-open.png"
 }
