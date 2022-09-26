8cfa7514c10e42ab605ab7ef16124fa348ea2b28
collinjones <cojo2657@colorado.edu>
2022-09-25 18:22:13 -0600

    revert changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
+from types import NoneType
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.contrib.postgres.fields import ArrayField
+from bnSubmission.models import Bandname
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- bnSubmission/views.py
+++ bnSubmission/views.py
@@ -6,13 +6,37 @@ from django.contrib.auth.models import User
 from .utils import *
 from .form_functions import *
 
+# for every single bandname in database, 
+# go through every single voted_bandnames list and remove entries that 
+# don't exist anymore.
+
+def convert_current_user(user):
+    user.profile.voted_bandnames_2.add(Bandname.objects.get(pk=1))
+
+def deleted_bandname_cleanup():
+    bandnames = Bandname.objects.all()
+    cleaned_list = []
+    for bandname in bandnames:
+        cleaned_list.append(bandname.bandname)
+
+    users = User.objects.all()
+    for user in users:
+        voted_bandnames = user.profile.voted_bandnames
+        if voted_bandnames:
+            if not isinstance(voted_bandnames, str):
+                for key in voted_bandnames.copy():
+                    if key not in cleaned_list:
+                        print('deleting: ', key)
+                        del user.profile.voted_bandnames[key]
+        user.save()
+
 # Sets up and renders the submission page
 def index(request):
     
     form = CreateBandname()
     bandnames = []
     collection_len = Bandname.objects.count()
-    users_count = User.objects.count()
+    # users_count = User.objects.count()
     cleaned_list = []
     voted_bandnames_objs = []
     profanity_filter = True
@@ -26,8 +50,10 @@ def index(request):
     if request.user.is_authenticated:
 
         user = User.objects.get(pk=request.user.id)
+        convert_current_user(user)
         profanity_filter = user.profile.profanity_filter
         voted_bandnames = user.profile.voted_bandnames
+        print(voted_bandnames)
 
         # If user has voted on atleast one bandname, search for each bandname in the db
         if voted_bandnames is not None:
@@ -48,7 +74,6 @@ def index(request):
     if len(cleaned_list) == 0:
         cleaned_list.append("NO BANDNAMES AVAILABLE")
 
-    print(f'{users_count = }')
 
     ctxt = {
         "title"     : "Submission Page",
