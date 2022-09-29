2e6d082814bbb8180f9cd933643a0f16c3eac782
collinjones <cojo2657@colorado.edu>
2022-09-28 20:54:04 -0600

    fixed cursor across entire site

--- main/forms.py
+++ main/forms.py
@@ -6,13 +6,21 @@ class Vote(forms.Form):
     downvote = forms.CharField()
 
 class CreateBandname(forms.Form):
-    bandname = forms.CharField(label="Bandname", max_length=128, required=False, widget = forms.TextInput(attrs={'id':'bandname', 'class': 'text-cursor'}))
+    bandname = forms.CharField(
+        label="Bandname", 
+        max_length=128, required=False, 
+        widget = forms.TextInput(attrs={'id':'bandname', 'class': 'text-cursor'})
+    )
 
 class CreateBatchBandname(forms.Form):
-    bandnames = forms.CharField(required=True, label="", widget=forms.Textarea(attrs={'id':'bandnames', 
-                                                                                      'placeholder': "Bandname \
-                                                                                                    \n1. Bandname \
-                                                                                                    \nBandname (05/05/1984)\
-                                                                                                    \n1. Bandname (05/05/1984)"}))
+    bandnames = forms.CharField(
+        required=True, label="", 
+        widget=forms.Textarea(attrs={'id':'bandnames', 
+            'placeholder': "Bandname \
+            \n1. Bandname \
+            \nBandname (05/05/1984)\
+            \n1. Bandname (05/05/1984)"}
+        )
+    )
     numbered = forms.BooleanField(label="Are any of these bandnames numbered?", required=False)
     dated = forms.BooleanField(label="Are any of these bandnames dated?", required=False)
