7cfe61a1afd8e5785222fa97915ae47a4bfd6cc2
collinjones <cojo2657@colorado.edu>
2022-10-24 08:35:55 -0600

    added line to procfile to start celery worker

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web gunicorn BandnamesWebsite.wsgi:application --log-file -
+worker: celery -A BandnamesWebsite worker --beat
--- main/tasks.py
+++ main/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
-from celery import app
 
-@app.task
+shared_task()
 def check():
     print('hello-world')
