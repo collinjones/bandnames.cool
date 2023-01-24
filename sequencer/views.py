fe4e6e7837a3b7631972aead3ae201db5d2703a9
collinjones <cojo2657@colorado.edu>
2023-01-24 13:06:12 -0700

    fixed mobile bug

--- sequencer/views.py
+++ sequencer/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    if (request.device.is_mobile):
+    if (request.device['is_mobile']):
         return render(request, "../templates/sequencer/mobile.html")
     else:
         return render(request, "../templates/sequencer/base.html")
