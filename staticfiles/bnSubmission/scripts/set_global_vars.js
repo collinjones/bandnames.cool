8a0aeafb6a8f40c5cf67200fdaa2267d4900020a
collinjones <cojo2657@colorado.edu>
2023-01-23 17:59:26 -0700

    fixed

--- staticfiles/bnSubmission/scripts/set_global_vars.js
+++
@@ -1,9 +0,0 @@
-var bandnames = []
-'{% for bandname in bandnames %}'
-    if ("{{profanity_filter}}" == "True"){
-        bandnames.push('{{bandname|censor}}')
-    }
-    else {
-        bandnames.push('{{bandname}}')
-    }
-'{% endfor %}'
