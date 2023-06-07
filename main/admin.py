d3a752816a3a46f8fc1be781794c7b2b9acc0599
collinjones <cojo2657@colorado.edu>
2023-06-07 15:59:58 -0600

    added searchbar in django admin for bandnames

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Bandname
 
+class BandnameAdmin(admin.ModelAdmin):
+    search_fields = ['bandname']
+
 # Register your models here.
-admin.site.register(Bandname)
+admin.site.register(Bandname, BandnameAdmin)
