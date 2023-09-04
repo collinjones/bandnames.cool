67063fd5b1d5904238de6b098893eb9b5fe7809d
collinjones <cojo2657@colorado.edu>
2023-09-03 19:24:24 -0600

    added ability to sort bandnames columns in admin console

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,7 @@ from .models import Bandname
 
 class BandnameAdmin(admin.ModelAdmin):
     search_fields = ['bandname']
+    list_display = ('bandname', 'score', 'date_submitted', 'username')
 
 # Register your models here.
 admin.site.register(Bandname, BandnameAdmin)
