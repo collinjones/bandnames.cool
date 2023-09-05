67e022c3e725c9ae7eb72f85e7f9668735dc2919
collinjones <cojo2657@colorado.edu>
2023-09-05 17:42:03 -0600

    Added ability to sort users by date joined in admin console

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from accounts.models import Profile
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class MyUserAdmin(UserAdmin):
+    # override the default sort column
+    ordering = ('date_joined', )
+    # if you want the date they joined or other columns displayed in the list,
+    # override list_display too
+    list_display = ('username', 'email', 'date_joined', 'first_name', 'last_name', 'is_staff')
+
 # Register your models here.
 admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
