2d2e9873e795c0d00b65d59a295f28fa36b80ec6
collinjones <cojo2657@colorado.edu>
2023-12-01 06:31:40 -0700

    Removed unused print statement in accounts/views.py

---
+++ staticfiles/registration/scripts/delete_bandname.js
@@ -0,0 +1,20 @@
+$("[id^=delete-button-profile]").click(function(e) {
+    e.preventDefault(); // Stop page from refreshing
+    $.blockUI({ message: "Deleting bandname... This may take a while..." }); 
+    $.ajax({
+        type: 'POST',
+        url: '/delete_bandname',
+        data: {
+            bandname: $(this).attr("value"),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        },
+        success: function (data) {
+            
+            if (data.hasOwnProperty('response_msg')){
+                $('#submission-status').html(data['response_msg']);
+            }  
+            document.getElementById(data['bandname']).remove();
+            $.unblockUI();
+        }
+    });
+});
